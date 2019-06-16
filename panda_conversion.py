dc8215b439ed70c14287c2b55040f9095c1dd8c2
maartenelgar <40140464+maartenelgar@users.noreply.github.com>
2019-06-16 12:21:15 +0200

    Update panda_conversion.py

--- panda_conversion.py
+++ panda_conversion.py
@@ -1,7 +1,18 @@
+import numpy
+import pandas as pd
+from pyq import q
+
 
 def df2kdb():
-
-
-f = pd.DataFrame.from_dict({'a': [1,2], 'b':[3,4]})
-t = q('!', f.columns, f.values.T).flip
-t.show()
+  '''
+  converts a panda dataframe built up from a dictionary to a 
+  '''
+  f = pd.DataFrame.from_dict({'a': [1,2], 'b':[3,4]})
+  t = q('!', f.columns, f.values.T).flip
+  t.show()
+  
+  
+def df2kdb_fast():
+  df = pd.DataFrame.from_dict({'a': [1,2], 'b':[3,4]})
+  t = q('!', df.columns, [numpy.array(df[x]) for x in df.columns]).flip
+  t.show()
